d9f09d6683e5aad07e3f762e7d17f57f5d342871
dengxiaobing <dengxiaobing@bytedance.com>
2021-03-30 13:00:19 +0800

    创建demo精灵

--- core/src/com/benson/study/libgdx/develop/guide/main/WorldController.kt
+++ core/src/com/benson/study/libgdx/develop/guide/main/WorldController.kt
@@ -1,12 +1,54 @@
 package com.benson.study.libgdx.develop.guide.main
 
+import com.badlogic.gdx.graphics.Pixmap
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.math.MathUtils
+
 class WorldController {
 
-    private fun init() {
+    companion object {
+        private const val SPRITE_COUNT = 5 // 精灵个数
+        private const val SPRITE_SIZE = 32 // 精灵大小
+    }
 
+    lateinit var testSprites: Array<Sprite>
+    private var selectedSprite = 0
+
+    init {
+        initSprites()
     }
 
     fun update(delta: Float) {
+        updateSelectedSprite(delta)
+    }
 
+    private fun initSprites() {
+        testSprites = Array(SPRITE_COUNT) { index ->
+            Sprite(Texture(Pixmap(SPRITE_SIZE, SPRITE_SIZE, Pixmap.Format.RGB888).apply {
+                setColor(1F, 0F, 0F, 0.5F)
+                fill()
+
+                setColor(1F, 1F, 0F, 1F)
+                drawLine(0, 0, width, height)
+                drawLine(width, 0, 0, height)
+
+                setColor(0F, 1F, 1F, 1F)
+                drawRectangle(0, 0, width, height)
+            })).apply {
+                setSize(1.0F, 1.0F)
+                setOrigin(width / 2, height / 2)
+                setPosition(MathUtils.random(-2.0F, 2.0F), MathUtils.random(-2.0F, 2.0F))
+            }
+        }
+    }
+
+    private fun updateSelectedSprite(delta: Float) {
+        testSprites[selectedSprite].apply {
+            var newRotation = rotation
+            newRotation += 90 * delta
+            newRotation %= 360
+            rotation = newRotation
+        }
     }
 }
--- core/src/com/benson/study/libgdx/develop/guide/main/WorldRender.kt
+++ core/src/com/benson/study/libgdx/develop/guide/main/WorldRender.kt
@@ -1,22 +1,40 @@
 package com.benson.study.libgdx.develop.guide.main
 
+import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.utils.Disposable
+import com.benson.study.libgdx.develop.guide.config.GameConfigConstant
 
 class WorldRender(private val controller: WorldController) : Disposable {
 
-    private fun init() {
-
+    private val batch: Batch = SpriteBatch()
+    private val camera: Camera = OrthographicCamera(GameConfigConstant.VIEWPORT_WIDTH, GameConfigConstant.VIEWPORT_HEIGHT).apply {
+        position.set(Vector3(0F, 0F, 0F))
+        update()
     }
 
     fun resize(width: Int, height: Int) {
-
+        camera.viewportHeight = GameConfigConstant.VIEWPORT_HEIGHT / height * width
+        camera.update()
     }
 
     fun render() {
+        batch.projectionMatrix = camera.combined
+        batch.begin()
+        renderSprites()
+        batch.end()
+    }
 
+    private fun renderSprites() {
+        controller.testSprites.forEach {
+            it.draw(batch)
+        }
     }
 
     override fun dispose() {
-
+        batch.dispose()
     }
 }
