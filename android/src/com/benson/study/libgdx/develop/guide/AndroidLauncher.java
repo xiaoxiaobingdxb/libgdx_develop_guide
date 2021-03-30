0e5574207df400aab8cca7922917e8ae9047adcc
dengxiaobing <dengxiaobing@bytedance.com>
2021-03-30 09:03:12 +0800

    init

---
+++ android/src/com/benson/study/libgdx/develop/guide/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.benson.study.libgdx.develop.guide;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.benson.study.libgdx.develop.guide.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
---
+++ desktop/src/com/benson/study/libgdx/develop/guide/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.benson.study.libgdx.develop.guide.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.benson.study.libgdx.develop.guide.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
